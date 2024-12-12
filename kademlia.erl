ff826c733f8f539f7a7c13a8a140853504258dde
Nicholas Kania <nicholas.nick.kania.development@gmail.com>
2024-12-12 16:21:59 +0100

    Project Update
    > Added kademlia.erl with the interface needed

---
+++ kademlia.erl
@@ -0,0 +1,26 @@
+-module(kademlia).
+-export([
+    
+]).
+
+
+
+ping(Id) ->
+    todo
+.
+
+store(Key, Value) ->
+    todo
+.
+
+find_node(Id) ->
+    todo
+.
+
+find_value(Key) ->
+    todo
+.
+
+join(Id) ->
+    todo
+.
